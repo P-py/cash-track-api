7246f63a45ede53b636d53f1eba9dd12f4b8857e
Pedro <pedrosalviano170@gmail.com>
2025-03-19 16:06:07 -0300

    :tada: start project

---
+++ src/test/kotlin/com/cashtrack/cashtrack_api/CashtrackApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cashtrack.cashtrack_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CashtrackApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
