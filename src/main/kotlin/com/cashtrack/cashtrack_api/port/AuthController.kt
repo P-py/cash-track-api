9677ff537265988b3acda07204a0da3552929d72
Pedro <pedrosalviano170@gmail.com>
2025-03-20 10:45:47 -0300

    :lock: add configuration and JWT properties + controller

---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/port/AuthController.kt
@@ -0,0 +1,19 @@
+package com.cashtrack.cashtrack_api.port
+
+import com.cashtrack.cashtrack_api.application.service.AuthenticationService
+import com.cashtrack.cashtrack_api.domain.auth.request.AuthenticationRequest
+import com.cashtrack.cashtrack_api.domain.auth.response.AuthenticationResponse
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@CrossOrigin
+@RequestMapping("/auth")
+class AuthController(
+    private val authenticationService:AuthenticationService,
+){
+    @PostMapping
+    fun authenticate(@RequestBody authRequest: AuthenticationRequest): AuthenticationResponse {
+        val authResponse = authenticationService.authentication(authRequest)
+        return authResponse
+    }
+}
