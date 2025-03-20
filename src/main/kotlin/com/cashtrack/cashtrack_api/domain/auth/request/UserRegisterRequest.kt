bbbb2ed27d9d9462915a902c26e155cc4634ecb7
Pedro <pedrosalviano170@gmail.com>
2025-03-20 11:49:01 -0300

    :sparkles::lock: add register controller
    Co-authored-by: erickrb8 <erick.ribeirodw@gmail.com>
    Co-authored-by: Luiz Gustavo Motta Viana <236428@facens.br>
    Co-authored-by: Ronaldo Simeone <ronaldosimeon3@gmail.com>

---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/auth/request/UserRegisterRequest.kt
@@ -0,0 +1,19 @@
+package com.cashtrack.cashtrack_api.domain.auth.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+
+data class UserRegisterRequest(
+    @field:NotEmpty(message = "Email must not be empty.")
+    @field:NotBlank(message = "Email must not be blank.")
+    @field:Size(min = 5, max = 100, message = "Email size must be between 5 and 100 characters.")
+    val email:String,
+    @field:NotEmpty(message = "Username must not be empty.")
+    @field:NotBlank(message = "Username must not be blank.")
+    @field:Size(min = 3, max = 40, message = "Username size must be between 3 and 40.")
+    val username:String,
+    @field:NotEmpty(message = "Password must no be empty.")
+    @field:NotBlank(message = "Password must no be blank.")
+    val password:String
+)
---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/error/exception/UserAlreadyExistsException.kt
@@ -0,0 +1,3 @@
+package com.cashtrack.cashtrack_api.domain.error.exception
+
+class UserAlreadyExistsException(message:String?):RuntimeException(message)
