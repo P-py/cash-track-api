9b4a4005442ea5168ae1e5be23b57ad620cb38b8
Pedro <pedrosalviano170@gmail.com>
2025-03-20 10:46:46 -0300

    :sparkles::passport_control: add service logic for authentication

---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/application/service/AuthenticationService.kt
@@ -0,0 +1,37 @@
+package com.cashtrack.cashtrack_api.application.service
+
+import com.cashtrack.cashtrack_api.domain.auth.request.AuthenticationRequest
+import com.cashtrack.cashtrack_api.domain.auth.response.AuthenticationResponse
+import com.cashtrack.cashtrack_api.domain.repository.UserRepository
+import com.cashtrack.cashtrack_api.port.config.property.JwtProperties
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.stereotype.Service
+import java.util.Date
+
+@Service
+class AuthenticationService(
+    private val authManager: AuthenticationManager,
+    private val userDetailsService:CustomUserDetailsService,
+    private val tokenService:TokenService,
+    private val jwtProperties:JwtProperties,
+    private val usersRepository: UserRepository,
+){
+    fun authentication(authRequest:AuthenticationRequest): AuthenticationResponse {
+        authManager.authenticate(
+            UsernamePasswordAuthenticationToken(
+                authRequest.email,
+                authRequest.password
+            )
+        )
+        val user = userDetailsService.loadUserByUsername(authRequest.email)
+        val accessToken = tokenService.generate(
+            userDetails = user,
+            expirationDate = Date(System.currentTimeMillis() + jwtProperties.accessTokenExpiration)
+        )
+        return AuthenticationResponse(
+            accessToken = accessToken,
+            userId = usersRepository.findByEmail(user.username)?.id
+        )
+    }
+}
---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/application/service/CustomUserDetailsService.kt
@@ -0,0 +1,26 @@
+package com.cashtrack.cashtrack_api.application.service
+
+import com.cashtrack.cashtrack_api.domain.UserCashtrack
+import com.cashtrack.cashtrack_api.domain.repository.UserRepository
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+typealias ApplicationUser = UserCashtrack
+
+@Service
+class CustomUserDetailsService(
+    private val usersRepository:UserRepository,
+): UserDetailsService {
+    override fun loadUserByUsername(username:String): UserDetails =
+        usersRepository.findByEmail(username)?.mapToUserDetails()
+            ?: throw RuntimeException("User not found!")
+
+    private fun ApplicationUser.mapToUserDetails(): UserDetails =
+        User.builder()
+            .username(this.email)
+            .password(this.password)
+            .roles(this.role.name)
+            .build()
+}
