4a66faefa6db091e82424cbaa561bf7e1718b1f6
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-14 17:28:25 -0300

    :goal_net: add error handling files

--- src/main/kotlin/com/cashtrack/cashtrack_api/domain/error/exception/CustomException.kt
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/error/exception/CustomException.kt
@@ -3,7 +3,6 @@ package com.cashtrack.cashtrack_api.domain.error.exception
 import com.cashtrack.cashtrack_api.domain.error.enum.ExceptionEnum
 import org.springframework.http.HttpStatus
 
-@Suppress("PackageNaming", "")
 open class CustomException(
     open val errorEnum: ExceptionEnum,
     open val code: String = errorEnum.code ?: errorEnum.name,
