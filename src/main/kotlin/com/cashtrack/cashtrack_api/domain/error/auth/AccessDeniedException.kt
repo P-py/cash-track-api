e710817dbe7b7643b5516e6d6dacf0020941cc70
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-14 19:40:36 -0300

    :recycle: add new exceptions for user service logic

---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/error/auth/AccessDeniedException.kt
@@ -0,0 +1,11 @@
+package com.cashtrack.cashtrack_api.domain.error.auth
+
+import com.cashtrack.cashtrack_api.domain.error.enum.ExceptionEnum
+import com.cashtrack.cashtrack_api.domain.error.exception.CustomException
+
+open class AccessDeniedException(
+    override val errorEnum: ExceptionEnum = ExceptionEnum.ACCESS_DENINED,
+    val data: String? = null,
+    override val code: String = errorEnum.code,
+    override val message: String = "Você não tem permissão para acessar esse recurso",
+) : CustomException(errorEnum)
