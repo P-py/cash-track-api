572e48ae67e6cabe15017e53410267ca63a13417
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-14 19:20:25 -0300

    :goal_net: catch empty optional errors

---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/error/exception/DatabaseRegisterNotFoundException.kt
@@ -0,0 +1,10 @@
+package com.cashtrack.cashtrack_api.domain.error.exception
+
+import com.cashtrack.cashtrack_api.domain.error.enum.ExceptionEnum
+
+open class DatabaseRegisterNotFoundException(
+    override val errorEnum: ExceptionEnum = ExceptionEnum.DATABASE_REGISTER_NOT_FOUND,
+    val data: String? = null,
+    override val code: String = errorEnum.code,
+    override val message: String = "Registro não encontrado $data",
+) : CustomException(errorEnum)
