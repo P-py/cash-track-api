2b29752b3595546f03042742c672a54a16950d71
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-14 21:53:11 -0300

    :ambulance::bug: add component annotation on adapter class to fix deploy

--- src/main/kotlin/com/cashtrack/cashtrack_api/domain/adapter/ExpenseAdapter.kt
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/adapter/ExpenseAdapter.kt
@@ -3,7 +3,9 @@ package com.cashtrack.cashtrack_api.domain.adapter
 import com.cashtrack.cashtrack_api.domain.Expense
 import com.cashtrack.cashtrack_api.domain.dto.request.ExpenseRegisterRequest
 import com.cashtrack.cashtrack_api.domain.dto.response.ExpenseResponse
+import org.springframework.stereotype.Component
 
+@Component
 class ExpenseAdapter : Adapter<Expense, ExpenseResponse, ExpenseRegisterRequest> {
     override fun mapEntry(e: ExpenseRegisterRequest):Expense {
         return Expense(
