0a51dcd8d0d5b1ee1d5341f3d3befa837870a770
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-18 14:11:12 -0300

    :sparkles: add service logic and endpoint to account management
    Co-authored-by: Ronaldo Simeone <ronaldosimeon3@gmail.com>

--- src/main/kotlin/com/cashtrack/cashtrack_api/application/service/UserService.kt
+++ src/main/kotlin/com/cashtrack/cashtrack_api/application/service/UserService.kt
@@ -3,13 +3,18 @@ package com.cashtrack.cashtrack_api.application.service
 import com.cashtrack.cashtrack_api.application.extension.extractTokenValue
 import com.cashtrack.cashtrack_api.domain.adapter.UserAdapter
 import com.cashtrack.cashtrack_api.domain.dto.request.UserRegisterRequest
+import com.cashtrack.cashtrack_api.domain.dto.request.UserUpdateRequest
 import com.cashtrack.cashtrack_api.domain.dto.response.BalanceResponse
 import com.cashtrack.cashtrack_api.domain.dto.response.UserResponse
 import com.cashtrack.cashtrack_api.domain.error.auth.AccessDeniedException
+import com.cashtrack.cashtrack_api.domain.error.enum.ExceptionEnum
+import com.cashtrack.cashtrack_api.domain.error.exception.CustomException
 import com.cashtrack.cashtrack_api.domain.error.exception.DatabaseRegisterNotFoundException
 import com.cashtrack.cashtrack_api.domain.error.exception.NotFoundException
 import com.cashtrack.cashtrack_api.domain.error.exception.UserAlreadyExistsException
 import com.cashtrack.cashtrack_api.domain.repository.UserRepository
+import io.netty.util.internal.SuppressJava6Requirement
+import org.hibernate.sql.Update
 import org.springframework.orm.jpa.JpaObjectRetrievalFailureException
 import org.springframework.stereotype.Service
 
@@ -85,6 +90,64 @@ class UserService(
             return mapper.mapView(usersRepository.getReferenceById(userId ?: id))
         } catch (e:JpaObjectRetrievalFailureException){
             throw(NotFoundException(message = "Oh, something went wrong!! User not found!"))
+        }
+    }
+
+    @Suppress("ThrowsCount", "NestedBlockDepth")
+    fun updateUserData(updatedUser: UserUpdateRequest, accessToken: String): UserResponse {
+        val tokenValue = accessToken.extractTokenValue()
+        val userEmail = tokenService.extractEmail(tokenValue)
+
+        val userId = usersRepository.findByEmail(userEmail)
+            .orElseThrow { DatabaseRegisterNotFoundException(message = "Oh, something went wrong!! User not found!") }
+            .id ?: throw AccessDeniedException(message = "You don't have permission to access this page.")
+
+        try {
+            val toUpdate = usersRepository.getReferenceById(userId)
+            updatedUser.username?.let {
+                if (updatedUser.username != toUpdate.username) {
+                    toUpdate.username = updatedUser.username
+                } else {
+                    throw CustomException(
+                        errorEnum = ExceptionEnum.USERNAME_CANNOT_BE_EQUAL
+                    )
+                }
+            }
+            updatedUser.email?.let {
+                if (updatedUser.email != toUpdate.email) {
+                    val userExists = usersRepository.findByEmail(updatedUser.email).isPresent
+
+                    if (userExists) {
+                        throw UserAlreadyExistsException(
+                            message = "An user for this e-mail already exists."
+                        )
+                    }
+                    toUpdate.email = updatedUser.email
+                } else {
+                    throw CustomException(
+                        errorEnum = ExceptionEnum.EMAIL_CANNOT_BE_EQUAL
+                    )
+                }
+            }
+            return mapper.mapView(toUpdate)
+        } catch (e:JpaObjectRetrievalFailureException){
+            throw(NotFoundException(message = "Oh, something went wrong!! User not found!"))
+        }
+    }
+
+    fun deleteUserAccount(accessToken: String) {
+        val tokenValue = accessToken.extractTokenValue()
+        val userEmail = tokenService.extractEmail(tokenValue)
+
+        val userId = usersRepository.findByEmail(userEmail)
+            .orElseThrow { DatabaseRegisterNotFoundException(message = "Oh, something went wrong!! User not found!") }
+            .id ?: throw AccessDeniedException(message = "You don't have permission to access this page.")
+
+        try {
+            val deletedUser = usersRepository.getReferenceById(userId)
+            usersRepository.delete(deletedUser)
+        } catch (e:JpaObjectRetrievalFailureException){
+            throw(NotFoundException(message = "You can't delete a user that does not exist!"))
         }
     }
 }
