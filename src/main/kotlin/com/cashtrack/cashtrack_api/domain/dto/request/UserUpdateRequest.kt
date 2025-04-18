1fd356c60b8b86799860ddb3df55680cb6cee933
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-18 14:08:20 -0300

    :sparkles: add request dto
    Co-authored-by: Ronaldo Simeone <ronaldosimeon3@gmail.com>

---
+++ src/main/kotlin/com/cashtrack/cashtrack_api/domain/dto/request/UserUpdateRequest.kt
@@ -0,0 +1,12 @@
+package com.cashtrack.cashtrack_api.domain.dto.request
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Size
+
+data class UserUpdateRequest(
+    @field:Size(min = 5, max = 100, message = "Email size must be between 5 and 100 characters.")
+    @field:Email(message = "Invalid email format.")
+    val email:String?,
+    @field:Size(min = 3, max = 40, message = "Username size must be between 3 and 40.")
+    val username:String?,
+)
