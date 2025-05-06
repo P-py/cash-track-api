9aae2c2dea34e39d7864711efae932e833aa96fc
Pedro S Santos <pedrosalviano170@gmail.com>
2025-05-05 21:15:21 -0300

    :white_check_mark: add jacoco coverage report

---
+++ src/test/kotlin/com/cashtrack/api/CashtrackApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cashtrack.api
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class CashtrackApiApplicationTests {
+    @Test
+    @Suppress("EmptyFunctionBlock")
+    fun contextLoads() {
+    }
+}
